d2b131d720defe5f121c6dc90e5394f38c8fe709
Jeff Glaum <jeffglaum@live.com>
2025-07-16 10:50:49 -0700

    explicitely set the router base path to root

--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,7 @@ pub fn App() -> impl IntoView {
         <Meta charset="UTF-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <Router>
+        <Router base="/">
             <Routes fallback=|| view! { NotFound }>
                 <Route path=path!("/home") view=Home />
                 <Route path=path!("/getting-started") view=GettingStarted />
